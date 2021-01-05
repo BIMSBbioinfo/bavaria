03cc010ffdccec935934f3eccf23723271404259
wkopp <wolfgang.kopp@mdc-berlin.de>
2021-01-05 11:49:45 +0100

    added get_variable_regions added MultiLogisticRegression added BCVAE3 batch-adversarial variational auto-encoder added BCVAE2 variational auto-encoder with input batch label for batch correction

--- src/nmvae/utils.py
+++ src/nmvae/utils.py
@@ -450,18 +450,358 @@ class BCVAE(keras.Model):
         z = self.encoder(data)
         pred = self.decoder([z, data, labels])
         return pred
-        #batchpred = self.batcher(z)
-
-        #if not isinstance(batchpred, tuple):
-        #    batchpred = (batchpred,)
-        #batch_loss = []
-        #for i, (pred, labs) in enumerate(zip(batchpred, labels)):
-        #    batch_loss.append(self.cce(labs, tf.math.reduce_mean(pred, axis=1)))
-
-        #batch_loss = sum(batch_loss)
-        #total_loss = sum(self.encoder.losses) + sum(self.decoder.losses) - batch_loss
-
-        #losses['loss'] = total_loss
-        #return losses
 
 
+class BCVAE2(keras.Model):
+    """ Variational auto-encoder using the batch label as input condition"""
+    def __init__(self, encoder, decoder, **kwargs):
+        super(BCVAE2, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.encoder_predict = keras.Model(self.encoder.inputs,
+                                           self.encoder.get_layer('z_mean').output)
+
+        self.decoder = decoder
+
+    def save(self, filename):
+        if len(os.path.dirname(filename)) > 0:
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+        f = filename.split('.h5')[0]
+        s='.h5'
+        self.encoder.save(f + '_encoder_' + s)
+        self.decoder.save(f + '_decoder_' + s)
+
+    @classmethod
+    def create(cls, params, _create_encoder, _create_decoder):
+         encoder = _create_encoder(params)
+         decoder = _create_decoder(params)
+
+         return cls(encoder, decoder)
+
+    @classmethod
+    def load(cls, filename):
+        f = filename.split('.h5')[0]
+        s='.h5'
+        
+        custom_objects = {'Sampling': Sampling,
+                          'KLlossLayer': KLlossLayer,
+                          'ClipLayer': ClipLayer,
+                          'NegativeMultinomialEndpoint': NegativeMultinomialEndpoint,
+                          'AddBiasLayer': AddBiasLayer,
+                          'ScalarBiasLayer':ScalarBiasLayer,
+                         }
+        encoder = load_model(f + '_encoder_' + s, custom_objects=custom_objects)
+        decoder = load_model(f + '_decoder_' + s, custom_objects=custom_objects)
+        return cls(encoder, decoder)
+
+    def save_weights(self, filename, overwrite=True, save_format=None):
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        f = filename.split('.h5')[0]
+        s='.h5'
+
+        self.encoder.save_weights(f + '_encoder_' + s)
+        self.decoder.save_weights(f + '_decoder_' + s)
+
+    def load_weights(self, filename, by_name=False, skip_mismatch=False):
+        f = filename.split('.h5')[0]
+        s='.h5'
+
+        self.encoder.load_weights(f + '_encoder_' + s)
+        self.decoder.load_weights(f + '_decoder_' + s)
+
+    def call(self, data):
+        if isinstance(data, tuple):
+            data, labels = data
+        z = self.encoder([data, labels])
+        if len(self.decoder.losses) > 0:
+            pred = self.decoder([z, data, labels])
+        else:
+            pred = self.decoder(z)
+
+        return pred
+
+    def summary(self):
+        self.encoder.summary()
+        self.decoder.summary()
+
+    def train_step(self, data):
+        losses = dict()
+        if isinstance(data, tuple):
+            data, labels = data
+        with tf.GradientTape(persistent=True) as tape:
+            z = self.encoder([data, labels])
+            for i, loss in enumerate(self.encoder.losses):
+                losses[f'kl_loss_{i}'] = loss
+            pred = self.decoder([z, data, labels])
+            for i, loss in enumerate(self.decoder.losses):
+                losses[f'recon_loss_{i}'] = loss
+
+            total_loss = sum(self.encoder.losses) + sum(self.decoder.losses)
+
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        losses['loss'] = total_loss
+        return losses
+
+
+    def test_step(self, data):
+        losses = dict()
+        if isinstance(data, tuple):
+            data, labels = data
+        z = self.encoder([data, labels])
+        pred = self.decoder([z, data, labels])
+
+        total_loss = sum(self.encoder.losses) + sum(self.decoder.losses)
+
+        losses['loss'] = total_loss
+        return losses
+
+
+    def predict_step(self, data):
+        data = data[0]
+        if isinstance(data, tuple):
+            data, labels = data
+        z = self.encoder([data, labels])
+        pred = self.decoder([z, data, labels])
+        return pred
+
+class BCVAE3(keras.Model):
+    """ Variational auto-encoder using batch-adversarial training."""
+    def __init__(self, encoder, decoder, **kwargs):
+        super(BCVAE3, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.encoder_predict = keras.Model(self.encoder.get_layer('input_data').input,
+                                           self.encoder.get_layer('z_mean').output)
+
+        self.decoder = decoder
+
+        self.encoder_params = keras.Model(self.encoder.get_layer('input_data').input,
+                                          self.encoder.get_layer('random_latent').output).trainable_weights
+        self.batch_params = [w for w in encoder.trainable_weights if 'batchcorrect' in w.name]
+
+        ba = [l.output for l in self.encoder.layers if 'combine_batches' in l.name]
+        test_encoder = keras.Model(self.encoder.get_layer('input_data').input,
+                                   [self.encoder.get_layer('random_latent').output, ba], name="encoder")
+                                    #self.encoder.get_layer('concatenate').output], name="encoder")
+        self.test_encoder = test_encoder
+
+    def save(self, filename):
+        if len(os.path.dirname(filename)) > 0:
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+        f = filename.split('.h5')[0]
+        s='.h5'
+        self.encoder.save(f + '_encoder_' + s)
+        self.decoder.save(f + '_decoder_' + s)
+
+    @classmethod
+    def create(cls, params, _create_encoder, _create_decoder):
+         encoder = _create_encoder(params)
+         decoder = _create_decoder(params)
+
+         return cls(encoder, decoder)
+
+    @classmethod
+    def load(cls, filename):
+        f = filename.split('.h5')[0]
+        s='.h5'
+        
+        custom_objects = {'Sampling': Sampling,
+                          'KLlossLayer': KLlossLayer,
+                          'ClipLayer': ClipLayer,
+                          'BatchLoss': BatchLoss,
+                          'BatchKLLoss': BatchKLLoss,
+                          'ExpandDims': ExpandDims,
+                          'NegativeMultinomialEndpoint': NegativeMultinomialEndpoint,
+                          'AddBiasLayer': AddBiasLayer,
+                          'ScalarBiasLayer':ScalarBiasLayer,
+                         }
+        encoder = load_model(f + '_encoder_' + s, custom_objects=custom_objects)
+        decoder = load_model(f + '_decoder_' + s, custom_objects=custom_objects)
+        return cls(encoder, decoder)
+
+    def save_weights(self, filename, overwrite=True, save_format=None):
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        f = filename.split('.h5')[0]
+        s='.h5'
+
+        self.encoder.save_weights(f + '_encoder_' + s)
+        self.decoder.save_weights(f + '_decoder_' + s)
+
+    def load_weights(self, filename, by_name=False, skip_mismatch=False):
+        f = filename.split('.h5')[0]
+        s='.h5'
+
+        self.encoder.load_weights(f + '_encoder_' + s)
+        self.decoder.load_weights(f + '_decoder_' + s)
+
+    def call(self, data):
+        if isinstance(data, tuple):
+            data, labels = data
+        z = self.encoder([data, labels])
+        if len(self.decoder.losses) > 0:
+            pred = self.decoder([z, data, labels])
+        else:
+            pred = self.decoder(z)
+
+        return pred
+
+    def summary(self):
+        self.encoder.summary()
+        self.decoder.summary()
+
+    def train_step(self, data):
+        losses = dict()
+        if isinstance(data, tuple):
+            profile, labels = data
+
+        with tf.GradientTape(persistent=True) as tape:
+            z, b = self.encoder(data)
+            kl_loss, batch_loss = self.encoder.losses
+            print(self.encoder.losses)
+            losses['kl_loss'] = kl_loss
+            losses['bloss'] = batch_loss
+            #losses['batch_skl'] = bskl
+            #losses['batch_kl'] = bkl
+
+            pred = self.decoder([z, profile, labels])
+            for i, loss in enumerate(self.decoder.losses):
+                losses[f'recon_loss_{i}'] = loss
+
+            recon_loss = sum(self.decoder.losses)
+            total_loss = kl_loss + recon_loss - batch_loss
+
+        grads = tape.gradient(total_loss, self.encoder_params + self.decoder.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.encoder_params + self.decoder.trainable_weights))
+
+        grads = tape.gradient(batch_loss, self.batch_params)
+        self.optimizer.apply_gradients(zip(grads, self.batch_params))
+
+        del tape
+        losses['loss'] = total_loss
+        for mn, s in zip(self.encoder.metrics_names, self.encoder.metrics):
+            losses[mn] = s.result()
+        return losses
+
+
+    def test_step(self, data):
+        losses = dict()
+        if isinstance(data, tuple):
+            profile, labels = data
+        #z = self.encoder(data)
+        z, b = self.encoder(data)
+        #kl_loss, batch_loss = self.encoder.losses
+        kl_loss, batch_loss = self.encoder.losses
+        pred = self.decoder([z, profile, labels])
+        recon_loss = sum(self.decoder.losses)
+
+        total_loss = kl_loss + recon_loss - batch_loss
+
+        losses['loss'] = total_loss
+        for mn, s in zip(self.encoder.metrics_names, self.encoder.metrics):
+            losses[mn] = s.result()
+        return losses
+
+
+class MultiLogisticRegression:
+    def __init__(self, adata, cluster, batches):
+        self.cluster = cluster
+        self.batches = batches
+        nregions = adata.shape[-1]
+        nclusters = adata.obsm[cluster].shape[-1]
+        nbatches = sum([adata.obsm[b].shape[-1] for b in batches])
+        self.optimizer = tf.keras.optimizers.Adam()
+        init = tf.random_normal_initializer(stddev=.001)
+        self.bce = tf.keras.losses.BinaryCrossentropy()
+        self.W = tf.Variable(init(shape=(nregions,nclusters)))
+        self.B = tf.Variable(init(shape=(nregions,nbatches)))
+        self.R = tf.Variable( init(shape=(nregions,)))
+        self.a = tf.Variable( init(shape=(nregions,1)))
+
+    @property
+    def params(self):
+        return [self.W,self.B,self.R,self.a]
+
+    def loss(self, y, pred):
+        y_pos, y_neg = y
+        return - tf.math.reduce_mean(tf.math.xlogy(y_pos, pred)) - \
+               tf.math.reduce_mean(tf.math.xlogy(y_neg, 1.-pred)) + \
+               1e-4*tf.math.reduce_mean(tf.math.square(self.W))
+
+    def predict(self, inputs):
+        xc, xb, xr = inputs
+
+        l1 = tf.einsum('rk,mk->rm',self.W, xc)
+        l2 = tf.einsum('rb,mb->rm', self.B, xb)
+        l3 = tf.einsum('r,m->rm', self.R, xr)
+
+        return tf.math.sigmoid(l1 + l2 + l3 + self.a)
+
+    def update(self, inputs, y):
+
+        with tf.GradientTape(persistent=True) as tape:
+            tot_loss = self.loss(y, self.predict(inputs))
+
+        g = tape.gradient(tot_loss, self.params)
+        self.optimizer.apply_gradients(zip(g, self.params))
+
+        return tot_loss
+
+    def fit(self, adata, epochs=10):
+
+        clusters = adata.obsm[self.cluster]
+        nbatches = len(self.batches)
+        batches = np.concatenate([adata.obsm[batch] for batch in self.batches], axis=1)
+        
+        #c x m
+        M = np.concatenate([clusters, batches],axis=1)
+
+        # m x m
+        Mmap = pd.DataFrame(M).drop_duplicates().values
+        M = M.dot(Mmap.T)
+        # c x m  cell to cluster/batch combination
+        M[M<(1+nbatches)] = 0
+        M[M>=(1+nbatches)] = 1
+
+        # region by cells x cells by M
+        y_pos = adata.X.T.dot(M)
+        y_neg = np.ones(adata.shape[0]).dot(M) - y_pos
+
+        # m x b
+        M2kmap = Mmap[:,:clusters.shape[1]]
+        M2bmap = Mmap[:,clusters.shape[1]:]
+
+        # m x k
+        readdepth = adata.obs['rdepth'].values.astype('float32')/adata.shape[-1]
+        readdepth = M.T.dot(readdepth)
+
+        X = (M2kmap, M2bmap, readdepth)
+        y = (y_pos,y_neg)
+        for i in range(epochs):
+            loss = self.update(X, y)
+
+
+def get_variable_regions(adata, groupby="louvain",
+                         batches=['batch']):
+    """Extract variable regions relative to clustering."""
+
+    if 'readdepth' not in adata.obs.columns:
+        adata.obs['rdepth'] = np.asarray(adata.X.sum(1)).flatten()
+
+    if batches is None:
+        batches = ['dummybatch']
+        adata.obs['dummybatch'] = 'dummy'
+    for batch in batches or []:
+        if batch not in adata.obsm:
+            adata.obsm[batch] = OneHotEncoder(sparse=False).fit_transform(np.asarray(adata.obs[batch].values.tolist()).reshape(-1,1))
+    if groupby not in adata.obsm:
+        adata.obsm[groupby] = OneHotEncoder(sparse=False).fit_transform(np.asarray(adata.obs[groupby].values.tolist()).reshape(-1,1))
+
+    mlr = MultiLogisticRegression(adata, groupby, batches)
+    mlr.fit(adata, epochs=500)
+    scores = mlr.W.numpy()
+
+    np.testing.assert_equal(scores.shape, (adata.shape[1], adata.obsm[groupby].shape[1]))
+
+    adata.varm['cluster_spec_region'] = scores
+
+    return adata
